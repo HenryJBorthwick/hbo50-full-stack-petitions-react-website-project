d6fb5159bd9377f9de3be02098adde4aa280cb66
Henry Borthwick <hbo50@uclive.ac.nz>
2024-05-06 15:24:34 +1200

    Added a Register.tsx and updated the configs and env to work. Currently, can post a user.

--- config.ts
+++ config.ts
@@ -1,6 +1,10 @@
-import dotenv from 'dotenv'
-dotenv.config()
+// import dotenv from 'dotenv'
+// dotenv.config()
 
-export const API_HOST_DEPLOYED = "https://seng365.csse.canterbury.ac.nz/api/v1";
-export const API_HOST_LOCAL = "http://localhost:4941/api/v1";
-export const API_HOST = process.env.VITE_NODE_ENV === 'deployed' ? API_HOST_DEPLOYED : API_HOST_LOCAL;
+// export const API_HOST_DEPLOYED = "https://seng365.csse.canterbury.ac.nz/api/v1";
+// export const API_HOST_LOCAL = "http://localhost:4941/api/v1";
+// export const API_HOST = process.env.VITE_NODE_ENV === 'deployed' ? API_HOST_DEPLOYED : API_HOST_LOCAL;
+
+export const API_HOST_DEPLOYED = import.meta.env.VITE_API_HOST_DEPLOYED;
+export const API_HOST_LOCAL = import.meta.env.VITE_API_HOST_LOCAL;
+export const API_HOST = import.meta.env.VITE_NODE_ENV === 'deployed' ? API_HOST_DEPLOYED : API_HOST_LOCAL;
