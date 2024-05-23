3ba2f66311c9d0837e9046b86f6bdc11091e2cf9
Henry Borthwick <hbo50@uclive.ac.nz>
2024-05-23 14:59:43 +1200

    Trying to resolve source maps

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
-    sourcemap: true, // Enable source maps
-  }
+    sourcemap: true, // Enable source maps in production
+  },
+  css: {
+    devSourcemap: true, // Enable source maps in development
+  },
 });
